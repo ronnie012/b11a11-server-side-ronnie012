a62dae3e035e9ab5653ae9b685dfdf0e8c646f34
ronnie012 <ronnie.247365.1@gmail.com>
2025-06-08 18:11:24 +0600

    Implemented MongoDB database connection configuration.

---
+++ src/config/db.js
@@ -0,0 +1,27 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error('MONGODB_URI is not defined in .env file');
+}
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let db;
+
+async function connectDB() {
+  if (db) return db;
+  await client.connect();
+  db = client.db(process.env.DB_NAME || "tourZenDB"); // You can specify DB name in .env or use a default
+  console.log("Successfully connected to MongoDB!");
+  return db;
+}
+
+module.exports = { connectDB, client }; // Export client if you need to close connection explicitly
